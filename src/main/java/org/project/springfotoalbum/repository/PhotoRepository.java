77d32e27e03b89a49167d45753dc54adb894cb5f
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-04-13 13:00:35 +0200

    [ADD] REPOSITORY

---
+++ src/main/java/org/project/springfotoalbum/repository/PhotoRepository.java
@@ -0,0 +1,7 @@
+package org.project.springfotoalbum.repository;
+
+import org.project.springfotoalbum.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+}
