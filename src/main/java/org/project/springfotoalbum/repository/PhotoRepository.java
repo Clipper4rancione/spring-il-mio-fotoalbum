b0a2a7ff6846d5c2b4c61b089055a3052941a517
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-04-13 17:49:10 +0200

    [ADD] custom method for search

--- src/main/java/org/project/springfotoalbum/repository/PhotoRepository.java
+++ src/main/java/org/project/springfotoalbum/repository/PhotoRepository.java
@@ -3,5 +3,9 @@ package org.project.springfotoalbum.repository;
 import org.project.springfotoalbum.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+
+    public List<Photo> findByTitleContainingIgnoreCase(String title);
 }
