1ce2a7c888362c11866da631e11a25cd2f0a35b9
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-04-13 13:00:51 +0200

    [ADD] UPDATE CLASS

--- src/main/java/org/project/springfotoalbum/model/Photo.java
+++ src/main/java/org/project/springfotoalbum/model/Photo.java
@@ -11,8 +11,48 @@ public class Photo {
     private Integer id;
 
     private String title;
+    @Lob
     private String description;
     private String url;
     private boolean isVisible;
-    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
+    }
 }
