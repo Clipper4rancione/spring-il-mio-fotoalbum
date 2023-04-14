650fda8686523379d6b58c6e6333d2683c3420cc
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-04-14 16:38:59 +0200

    [ADD] INIT CRUD (CREATED SHOW)

--- src/main/java/org/project/springfotoalbum/controller/PhotoController.java
+++ src/main/java/org/project/springfotoalbum/controller/PhotoController.java
@@ -3,13 +3,17 @@ package org.project.springfotoalbum.controller;
 import org.project.springfotoalbum.model.Photo;
 import org.project.springfotoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/photos")
@@ -23,6 +27,18 @@ public class PhotoController {
         List<Photo> photos = photoRepository.findAll();
         model.addAttribute("photoList", photos);
         return "/photos/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Photo> result = photoRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("photo", result.get());
+            return "/photos/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo with id " + id + " not found");
+        }
+
     }
 
     @GetMapping("/search")
