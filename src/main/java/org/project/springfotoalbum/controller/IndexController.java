82564448cccdfe0d75ddf91bc245eeb4431224a8
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-04-14 15:42:23 +0200

    [ADD] NAVBAR

--- src/main/java/org/project/springfotoalbum/controller/IndexController.java
+++ src/main/java/org/project/springfotoalbum/controller/IndexController.java
@@ -1,8 +1,10 @@
 package org.project.springfotoalbum.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/")
