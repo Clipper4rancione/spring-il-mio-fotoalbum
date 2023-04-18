1d6f3cfee61820e49573c08d2c3e0060a464874f
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-04-18 12:20:39 +0200

    [ADD] updated rest

--- src/main/java/org/project/springfotoalbum/api/PhotoRestController.java
+++ src/main/java/org/project/springfotoalbum/api/PhotoRestController.java
@@ -1,15 +1,18 @@
 package org.project.springfotoalbum.api;
 
+import org.project.springfotoalbum.exceptions.PhotoNotFoundException;
 import org.project.springfotoalbum.model.Photo;
 import org.project.springfotoalbum.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/photos")
 public class PhotoRestController {
     @Autowired
@@ -17,7 +20,20 @@ public class PhotoRestController {
 
     //Lista di tutte le foto
     @GetMapping
-    public List<Photo> photoList() {
+    public List<Photo> photoList(@RequestParam(name = "q") Optional<String> search) {
+        if (search.isPresent()) {
+            return photoService.getFilteredPhotos(search.get());
+        }
         return photoService.getAllPhotos();
+    }
+
+    // Singola Foto
+    @GetMapping("/{id}")
+    public Photo getById(@PathVariable Integer id) {
+        try {
+            return photoService.getById(id);
+        } catch (PhotoNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
